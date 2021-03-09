bf2a0cb404f071ec5d7fc47e66200e769a878f4f
Reeggie Mace <reginaldmace36@gmail.com>
2021-03-09 08:43:28 -0500

    modified HTML added foundation for java script

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/* When the start button is clicked:
+A timer is started
+The user is presented with a question
+When the user answers the question he's presented another question
+If a question is answered incorrectly 10 seconds is subtracted from the clock
+When all questions have been answered or timer equals 0, the game is over.
+When the game is over user saves initials and score */
+
+var timer = document.querySelector("#timer");
+var questions = document.querySelector("#questions");
+var answers = document.querySelector("#answers");
+var startButton = document.querySelector("#start-button");
+
+// Set a timer to default setting when page loads
+function startTimer(){
+
+}
+//Generate random questions for the user to answer
+function generateQuestions {
+
+}
+//End the game when time is 0 or all questions are answered
+function gameOver {
+
+}
+//Save the results to local storage
+function saveResults {
+
+}
